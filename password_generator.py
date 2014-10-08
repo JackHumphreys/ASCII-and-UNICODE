76ba645f97176d4811ab5e67db4b7b987f641384
Jack <40618@longroad.ac.uk>
2014-10-08 22:56:52 +0100

    Password generator program

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+#Password Generator
+#Jack Humphreys
+
+import random
+pass_length = int(input("Please enter length of your password: "))
+for count in range(pass_length):
+    ran_choice1 = random.randint(48,57)
+    ran_choice2 = random.randint(65,90)
+    ran_choice3 = random.randint(97,122)
+    ran_list = [ran_choice1,ran_choice2,ran_choice3]
+    pw = random.choice(ran_list)
+    print(chr(pw))
