3439a7de70c78e4c9408193200f867568e3cc9e5
Jack <40618@longroad.ac.uk>
2014-10-20 22:02:17 +0100

    Password Generator

--- password_generator.py
+++ password_generator.py
@@ -2,6 +2,7 @@
 #Jack Humphreys
 
 import random
+
 pass_length = int(input("Please enter length of your password: "))
 for count in range(pass_length):
     ran_choice1 = random.randint(48,57)
@@ -9,4 +10,6 @@ for count in range(pass_length):
     ran_choice3 = random.randint(97,122)
     ran_list = [ran_choice1,ran_choice2,ran_choice3]
     pw = random.choice(ran_list)
-    print(chr(pw))
+    if pw
+    print(chr(pw),end="")
+
